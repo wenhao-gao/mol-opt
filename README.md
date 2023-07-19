386d1862f8bf3c78bb8ae1f3c294d7ed1ab81630
Tianfan Fu <tianfanfu@ipsec-10-2-0-113.vpn.gatech.edu>
2023-07-19 00:05:14 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# mol-opt
-Mol-Opt: a toolbox for molecular design
+# mol-opt: a toolbox for molecular design
+
+## 🚀 Installation 
+To install the necessary dependencies, run the following commands:
+
+```
+python setup.py install 
+```
+
+## 💻 Usage
+
+
+
+## 🤝 Contributing
+If you wish to contribute, please refer to the [CONTRIBUTING.md](CONTRIBUTING.md) file. We welcome pull requests for bug fixes, feature requests, and improvements to our code.
+
+
+## 📜 License
+This project is licensed under the terms of the [MIT License](LICENSE).
+
+
+## 💼 Support
+If you need help with the tool, you can raise an issue on our GitHub issue tracker. For other questions, please contact our team. 
+
+*Please note: mol-opt is under active development and certain functionalities are yet to be fully implemented.*
+
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+import setuptools
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open(os.path.join(this_directory, 'requirements.txt'),
+          encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name = 'mol-opt',
+    version = '0.0.1',
+    author = 'Wenhao Gao, Tianfan Fu, Jimeng Sun, Connor Coley',
+    author_email = 'gaowh19@gmail.com',
+    description = 'Mol-Opt: a toolbox for molecular design',
+    url = 'https://github.com/wenhao-gao/mol-opt',
+    keywords=['molecular optimization', 'drug discovery', 'drug design', 'artificial intelligence', 'deep learning', 'machine learning'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(exclude=['test']),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
